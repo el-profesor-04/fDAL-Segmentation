e1ca0bbc89b0f1ea6bfae1beb57f7ae10f6fe805
Fateme <fxg220004@utdallas.edu>
2023-06-29 11:36:40 -0500

    remove prev codes

--- run.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 screen -dmS b1 python train.py --beta=1.0
-CUDA_VISIBLE_DEVICES=2 screen -dmS b2 python train.py --beta=2.13
-CUDA_VISIBLE_DEVICES=3 screen -dmS b3 python train.py --beta=5.0
-CUDA_VISIBLE_DEVICES=4 screen -dmS b4 python train.py --beta=10.0
-CUDA_VISIBLE_DEVICES=5 screen -dmS b5 python train.py --beta=20.0
-CUDA_VISIBLE_DEVICES=6 screen -dmS b6 python train.py --beta=50.0
